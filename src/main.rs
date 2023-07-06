98493d0d88033b0ae5ac45930c73ce5498d20fee
Ole Schulz-Trieglaff <ole.st@gmx.de>
2023-07-06 11:20:24 +0100

    writing to vga buffer

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,20 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"Hello World!";
+
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // this tells the compiler that we are sure about these memory accesses
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
 
