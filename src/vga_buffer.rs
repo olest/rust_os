9767b1d0d37e9fe548abaa72b43401b9f3f4bce9
Ole Schulz-Trieglaff <ole.st@gmx.de>
2023-09-02 22:50:26 +0100

    talking to the VGA buffer

---
+++ src/vga_buffer.rs
@@ -0,0 +1,36 @@
+
+// disable warnings about unused code
+#[allow(dead_code)]
+// enable copy semantics for the type and make it printable and comparable.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// each enum variant is stored as a u8
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue = 1,
+    Green = 2,
+    Cyan = 3,
+    Red = 4,
+    Magenta = 5,
+    Brown = 6,
+    LightGray = 7,
+    DarkGray = 8,
+    LightBlue = 9,
+    LightGreen = 10,
+    LightCyan = 11,
+    LightRed = 12,
+    Pink = 13,
+    Yellow = 14,
+    White = 15,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// ensure that the ColorCode has the exact same data layout as a u8
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
