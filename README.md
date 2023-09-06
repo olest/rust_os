2677b60ad7586c28985bada6308a9e7fb06047c2
Ole Schulz-Trieglaff <ole.st@gmx.de>
2023-09-06 22:39:11 +0100

    adding volatile writes to VGA buffer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # rust_os
+
 A small operating system in Rust (based on https://os.phil-opp.com/)
+
+`cargo bootimage`
+
+`qemu-system-x86_64 -drive format=raw,file=./target/x86_64-rust_os/debug/bootimage-rust_os.bin`
+
+
+
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,9 @@
 
+// to prevent writes to VGA buffer from being optimized away
+use volatile::Volatile;
+// formatting 
+use core::fmt;
+
 // disable warnings about unused code
 #[allow(dead_code)]
 // enable copy semantics for the type and make it printable and comparable.
@@ -33,4 +38,88 @@ impl ColorCode {
     fn new(foreground: Color, background: Color) -> ColorCode {
         ColorCode((background as u8) << 4 | (foreground as u8))
     }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// same memory layout as a C struct
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode,
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+// same memory layout as the single 
+#[repr(transparent)]
+struct Buffer {
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer,
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH {
+                    self.new_line();
+                }
+
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+
+                let color_code = self.color_code;
+                self.buffer.chars[row][col].write(ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                });    
+                self.column_position += 1;
+            }
+        }
+    }
+
+    fn new_line(&mut self) {/* TODO */}
+
+        pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range
+                _ => self.write_byte(0xfe),
+            }
+
+        }
+    }
+
+}
+
+// implement the core::fmt::Write trait to support formatting macros
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+
+pub fn print_something() {
+
+    use core::fmt::Write;
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Yellow, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
 }
